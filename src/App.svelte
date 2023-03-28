9b17ff6a912adcf2304e68b0907c86c1e628feee
Rebecca Burch <rebecca.burch.stu@gmail.com>
2023-03-27 22:47:44 -0500

    new starter project from vite

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,49 @@
 <script>
-	export let name;
+  // @ts-ignore
+  import svelteLogo from './assets/svelte.svg'
+  // @ts-ignore
+  import viteLogo from './assets/vite.svg'
+  import Counter from './lib/Counter.svelte'
 </script>
 
 <main>
-	<h1>Hey you!</h1>
-	<p>Welcome to FlexFactor - the ultimate bulking calculator app designed to help you achieve your muscle-building goals! We're excited to have you on board and can't wait to see the gains you'll make with our personalized recommendations based on your body fat, biological sex, activity level, and strength standards. Let's get ready to flex!</p>
+  <div>
+    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+      <img src={viteLogo} class="logo" alt="Vite Logo" />
+    </a>
+    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
+      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
+    </a>
+  </div>
+  <h1>Vite + Svelte</h1>
+
+  <div class="card">
+    <Counter />
+  </div>
+
+  <p>
+    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
+  </p>
+
+  <p class="read-the-docs">
+    Click on the Vite and Svelte logos to learn more
+  </p>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: filter 300ms;
+  }
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+  .logo.svelte:hover {
+    filter: drop-shadow(0 0 2em #ff3e00aa);
+  }
+  .read-the-docs {
+    color: #888;
+  }
 </style>
