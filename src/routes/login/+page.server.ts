4899891133e45daba0f74c1f61476f772c413595
Rebecca Burch <rebecca.burch.stu@gmail.com>
2023-04-03 02:19:25 -0500

    no zod form

--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -1,21 +1,27 @@
 import type { Actions } from '@sveltejs/kit';
-
 import { fail, redirect } from '@sveltejs/kit';
-import { zfd } from 'zod-form-data';
 
 export const actions: Actions = {
 	default: async ({ request }) => {
 		const formData = await request.formData();
-		const loginSchema = zfd.formData({
-			user: zfd.text(),
-			password: zfd.text()
-		});
-		const result = loginSchema.safeParse(formData);
 
-		if (!result.success) {
+		const user = String(formData.get('user'));
+		const password = String(formData.get('password'));
+
+		const errors: Record<string, unknown> = {};
+
+		if (!user || typeof user !== 'string') {
+			errors.user = 'required';
+		}
+
+		if (!password || typeof password !== 'string') {
+			errors.password = 'required';
+		}
+
+		if (Object.keys(errors).length > 0) {
 			const data = {
 				data: Object.fromEntries(formData),
-				errors: result.error.flatten().fieldErrors
+				errors
 			};
 			return fail(400, data);
 		}
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { enhance } from '$app/forms';
 	import type { ActionData } from "./$types";
-let form: ActionData
+export let form: ActionData
 
 
 </script>
@@ -11,11 +11,11 @@ let form: ActionData
 <form method="POST" use:enhance>
 <input class="text-black" type="text" name="user" value={form?.data?.user ?? ''} />
 {#if form?.errors?.user}
-     <p>nooo</p>
+     <p class="text-red-400">nooo</p>
 {/if}
 <input class="text-black" type="password" name='password' value={form?.data?.password ?? ''} />
 {#if form?.errors?.password}
-     <p>nooo</p>
+     <p class="text-red-400">nooo</p>
 {/if}
 <button type="submit">Login</button>
 </form>
