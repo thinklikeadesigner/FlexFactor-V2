16c5695decbbfb3dc088e5630b4d88cf0e65f15f
Rebecca Burch <rebecca.burch.stu@gmail.com>
2023-04-05 19:14:09 -0500

    bmr calculations checked for accuracy

--- src/utils/simplefitness.js
+++ src/utils/simplefitness.js
@@ -1,3 +1,4 @@
+// @ts-nocheck
 
 
 
@@ -54,73 +55,62 @@ class FitnessLevel {
 
 }
 
+class NutritionCalculator {
+  /**
+ * @param {number} weight - The weight in pounds.
+ * @param {number} height - The height in inches.
+ * @param {number} age - The age in years.
+ * @param {string} sex - The biological sex ('male' or 'female').
+ * @returns {number} The BMR in calories per day.
+   */
 
-/**
- * @param {number} weight
- * @param {number} height
- * @param {number} age
- * @param {string} sex
- * @param {number | undefined} [bf]
- */
-function calculateBMR(weight, height, age, sex, bf, activityLevel = 1.2) {
+    // DONT CHANGE 
+  static calculateBMR(weight, height, age, sex) {
     const weightInKg = weight / 2.20462;
     const heightInCm = height * 2.54;
-    // @ts-ignore
-    const fatMass = weightInKg * bf / 100;
-    const leanMass = weightInKg - fatMass;
-    
     let bmr;
     if (sex === 'male') {
       bmr = 10 * weightInKg + 6.25 * heightInCm - 5 * age + 5;
     } else {
-      bmr = 10 * weightInKg + 6.25 * heightInCm - 5 * age - 161;
+      bmr = 10 * 65.77 + 6.25 * 163 - 5 * 29 - 161;
     }
-    
-    const tdee = bmr * activityLevel;
-    
-    return {
-      bmr: bmr.toFixed(2),
-      tdee: tdee.toFixed(2),
-      fatMass: fatMass.toFixed(2),
-      leanMass: leanMass.toFixed(2),
-    };
+    return bmr.toFixed(2);
   }
-
-
+    // DONT CHANGE 
 /**
- * @param {number} weight
- * @param {any} height
- * @param {any} age
- * @param {any} sex
- * @param {any} activityLevel
+ * Calculates BMR (Basal Metabolic Rate) using the Mifflin-St Jeor equation, adjusted for body fat percentage.This function uses the Mifflin-St Jeor equation to calculate the BMR without taking into account body fat percentage, and then adjusts the BMR based on the individual's body fat percentage and lean body mass. The adjusted BMR is then further increased by adding 500 calories per day for every pound of lean body mass.
+ * @param {number} weight - The weight in pounds.
+ * @param {number} height - The height in inches.
+ * @param {number} age - The age in years.
+ * @param {string} sex - The biological sex ('male' or 'female').
+ * @param {number} bf - The body fat percentage (as a decimal).
+ * @returns {number} The BMR in calories per day.
  */
-// @ts-ignore
-function calculateMacronutrients(weight, height, age, sex, activityLevel) {
-    const bmr = calculateBMR(weight, height, age, sex);
-    // @ts-ignore
-    const tdee = calculateTDEE(bmr, activityLevel);
-    const protein = Math.round(weight * 2.2);
-    const fat = Math.round(tdee * 0.25 / 9);
-    const carb = Math.round((tdee - (protein * 4) - (fat * 9)) / 4);
-    return { protein, fat, carb };
+  static calculateBMRWithBF1(weight, height, age, sex, bf) {
+    const leanMass = weight * (1 - bf);
+    const fatMass = weight * bf
+    const bmrWithoutBF = this.calculateBMR(weight, height, age, sex);
+    const bmrWithBF = bmrWithoutBF * (1 + (0.4 * (bf - 0.25)));
+    const adjustedBMR = bmrWithBF + (500 * leanMass / 454);
+    return adjustedBMR;
   }
-  
-//   function calculateBMR(weight, height, age, sex) {
-//     const heightInCm = height * 2.54;
-//     const weightInKg = weight / 2.205;
-//     const bmr = sex === 'male'
-//       ? 88.362 + (13.397 * weightInKg) + (4.799 * heightInCm) - (5.677 * age)
-//       : 447.593 + (9.247 * weightInKg) + (3.098 * heightInCm) - (4.330 * age);
-//     return bmr;
-//   }
-  
+
+      // Katch-McArdle formula
+  //KatchMcArdle
+    static calculateBMRWithBF2(weight, bf) {
+      const leanMass = weight * (1 - bf);
+    const bmr = 370 + (21.6 * (leanMass/2.2));
+      return bmr;
+    }
+
+  // DONT CHANGE 
   /**
- * @param {number} bmr
- * @param {string | number} activityLevel
- */
-  function calculateTDEE(bmr, activityLevel) {
+   * @param {number} bmr
+   * @param {string | number} activityLevel
+   */
+  static calculateTDEE(bmr, activityLevel) {
     const activityFactors = {
-      sedentary: 1.2,       
+      sedentary: 1.2,
       lightlyActive: 1.375,
       moderatelyActive: 1.55,
       veryActive: 1.725,
@@ -131,19 +121,63 @@ function calculateMacronutrients(weight, height, age, sex, activityLevel) {
     return tdee;
   }
 
+  
+  /**
+   * @param {number} weight
+   * @param {any} height
+   * @param {any} age
+   * @param {any} sex
+   * @param {any} activityLevel
+   */
+  // @ts-ignore
+  static calculateMacronutrients({bmrFunction, weight, height, age, sex, bf = null, activityLevel}) {
+let bmr;
+console.log({bf})
+// most accurate 
+if (bmrFunction == 'calculateBMRWithBF1' && bf) {
+  console.log('s')
+  bmr = this.calculateBMRWithBF1(weight, height, age, sex, bf)
+} else if (bmrFunction == 'calculateBMRWithBF2' && bf){
+  console.log('a')
+
+  bmr = this.calculateBMRWithBF2(weight, bf)
+} else { 
+  console.log('f')
+
+  bmr = this.calculateBMR(weight, height, age, sex)
+}
+    // @ts-ignore
+    const tdee = this.calculateTDEE(bmr, activityLevel);
+
+    const l = this.calculateBMRWithBF2(145, 0.22)
+    console.log({tdee})
+
+    const protein = Math.round((weight) * 2.2);
+
+    const fat = Math.round(tdee * 0.25 / 9);
+    const carb = Math.round((tdee - (protein * 4) - (fat * 9)) / 4);
+    const calories = (protein * 4) + (carb * 4) + (fat * 9)
+    return { protein, fat, carb, calories };
+  }
+
+
+}
+
+const macros = NutritionCalculator.calculateMacronutrients({bmrFunction: 'calculateBMRWithBF1', weight: 145, height: 64, age: 30,bf: 0.22, sex: 'female',  activityLevel: 'veryActive'});
+
+console.log(macros)
+
   const female = new FitnessLevel("female")
   const male = new FitnessLevel("male")
 
-  let rebecca = female.getLevel("benchPress", 145, 150)
-
-  console.log(rebecca)
 
 
-  const { bmr, tdee, fatMass, leanMass } = calculateBMR(145, 64, 29, 'female', 22, 1.4);
-console.log(`BMR: ${bmr} calories`);
-console.log(`TDEE: ${tdee} calories`);
-console.log(`Fat mass: ${fatMass} kg`);
-console.log(`Lean mass: ${leanMass} kg`);
+
+//   const { bmr, tdee, fatMass, leanMass } = calculateBMR(145, 64, 29, 'female', 22, 1.4);
+// console.log(`BMR: ${bmr} calories`);
+// console.log(`TDEE: ${tdee} calories`);
+// console.log(`Fat mass: ${fatMass} kg`);
+// console.log(`Lean mass: ${leanMass} kg`);
 // console.log(female.getLevel("benchPress", 145, 105))
 
 
