8caad3405581b426b83d20f42452c7535c160198
Devin Jaggernauth <neexis@gmail.com>
2023-04-10 12:06:25 -0400

    call determine P-ratio function

--- src/utils/PRatioDeterminer.ts
+++ src/utils/PRatioDeterminer.ts
@@ -3,9 +3,6 @@ import { pRatios } from './pRatios';
 interface SurplusHash {
 	[key: number]: string;
 }
-type Sex = 'male' | 'female';
-
-type FitnessLevel = 'beginner' | 'intermediate' | 'advanced';
 
 const surplusTable: SurplusHash = {
 	2.5: 'surplus2',
@@ -19,34 +16,35 @@ const getBodyFatRange = (sex: string, bodyFat: number): string => {
 	let bodyFatrange = '';
 	if (sex === 'female') {
 		if (bodyFat > 9 && bodyFat <= 19) bodyFatrange = 'bf12to19';
-		if (bodyFat > 20 && bodyFat <= 24) bodyFatrange = 'bf20to24';
-		if (bodyFat > 25 && bodyFat <= 31) bodyFatrange = 'bf25to35';
-		if (bodyFat > 32) bodyFatrange = 'bf32to40';
+		if (bodyFat >= 20 && bodyFat <= 24) bodyFatrange = 'bf20to24';
+		if (bodyFat >= 25 && bodyFat <= 31) bodyFatrange = 'bf25to35';
+		if (bodyFat >= 32) bodyFatrange = 'bf32to40';
 	}
 
 	if (sex === 'male') {
-		if (bodyFat > 4 && bodyFat <= 8) bodyFatrange = 'bf6to8';
-		if (bodyFat > 9 && bodyFat <= 13) bodyFatrange = 'bf9to12';
-		if (bodyFat > 13 && bodyFat <= 17) bodyFatrange = 'bf13to17';
-		if (bodyFat > 18) bodyFatrange = 'bf18to22';
+		if (bodyFat >= 4 && bodyFat <= 8) bodyFatrange = 'bf6to8';
+		if (bodyFat >= 9 && bodyFat <= 12) bodyFatrange = 'bf9to12';
+		if (bodyFat >= 13 && bodyFat <= 17) bodyFatrange = 'bf13to17';
+		if (bodyFat >= 18) bodyFatrange = 'bf18to22';
 	}
 
 	return bodyFatrange;
 };
 
 export const determinePRatio = (
-	sex: Sex = 'male',
+	sex = 'male',
 	calorieSurplus = 10,
 	bodyFat = 10,
-	fitnessLevel: FitnessLevel = 'intermediate'
+	fitnessLevel = 'intermediate'
 ): number => {
+	
 
 
 	const surplusClass = surplusTable[calorieSurplus];
 	const bodyFatrange = getBodyFatRange(sex, bodyFat);
 
-  if(sex === 'female' && bodyFat < 4) return 0;
-  if(sex === 'male' && bodyFat < 2) return 0;
+	if (sex === 'female' && bodyFat < 4) return 0;
+	if (sex === 'male' && bodyFat < 2) return 0;
 
 	return pRatios[sex][fitnessLevel][surplusClass][bodyFatrange] ?? 0;
 };
