da90d179cae83773a7b8634bad8ffa1d1fe1426b
Rebecca Burch <rebecca.burch.stu@gmail.com>
2023-04-15 15:33:04 -0500

    user can enters a value for 1rm starting from zero

--- src/utils/FitnessLevel.ts
+++ src/utils/FitnessLevel.ts
@@ -48,7 +48,7 @@ const getFitnessLevel = (
 
 	const fitnessLevel = Object.keys(section).find((level) => {
 		const [min, max]: number[] = section[level as keyof Section];
-		if (!min || !max) {
+		if (min === null || max === null || min === undefined || max === undefined) {
 			console.error('no min or max');
 		} else {
 			return oneRepMax >= min && oneRepMax <= max;
