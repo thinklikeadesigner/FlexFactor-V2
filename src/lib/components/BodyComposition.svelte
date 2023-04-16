824d31ebbc7c30c10fc65d43ac4a916660c15adf
Rebecca Burch <rebecca.burch.stu@gmail.com>
2023-04-16 15:52:15 -0500

    initialbodyfat changed to initialbodyfatpercent

--- src/lib/components/BodyComposition.svelte
+++ src/lib/components/BodyComposition.svelte
@@ -6,19 +6,26 @@
 	} from '../../utils/BodyCompositionResults';
 	import BodyStatsCard from './BodyStatsCard.svelte';
 
-	let { currentWeight, initialBodyFat, desiredGains, pratio } = $UserStore;
+	let { currentWeight, initialBodyFatPercent, desiredGains, pratio } = $UserStore;
 
-	const { endingWeight, endingBodyFatPercent } =
-		calculateBodyComposition(pratio, desiredGains, currentWeight, initialBodyFat) as BodyComposition;
-
-	
+	const { endingWeight, endingBodyFatPercent } = calculateBodyComposition(
+		pratio,
+		desiredGains,
+		currentWeight,
+		initialBodyFatPercent
+	) as BodyComposition;
 </script>
 
 <section class="w-11/12 m-auto">
 	<h2 class="mb-4">Body Composition Results</h2>
 	<p class="unstyled mb-4 font-medium text-lg text-center">Gaining {desiredGains}lbs of muscle</p>
 
-	<BodyStatsCard bodyFat={initialBodyFat} bodyWeight={currentWeight} title="Starting Stats" colour="primary" />
+	<BodyStatsCard
+		bodyFat={initialBodyFatPercent}
+		bodyWeight={currentWeight}
+		title="Starting Stats"
+		colour="primary"
+	/>
 
 	<BodyStatsCard
 		bodyFat={endingBodyFatPercent}
@@ -26,6 +33,5 @@
 		title="Ending Stats"
 		colour="secondary"
 	/>
-	<p class="unstyled mb-4 font-medium text-lg text-center">Time to gains: </p>
-
+	<p class="unstyled mb-4 font-medium text-lg text-center">Time to gains:</p>
 </section>
