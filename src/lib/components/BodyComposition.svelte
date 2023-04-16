456ccfe6a7bc9cdb17d8c6374219e1854a0fc70d
Devin Jaggernauth <neexis@gmail.com>
2023-04-16 00:10:58 -0400

    update result cards

--- src/lib/components/BodyComposition.svelte
+++ src/lib/components/BodyComposition.svelte
@@ -1,31 +1,49 @@
 <script lang="ts">
-	import { get } from 'svelte/store';
 	import UserStore from '../../stores/UserStore';
 	import {
 		calculateBodyComposition,
 		type BodyComposition
 	} from '../../utils/BodyCompositionResults';
+	import BodyStatsCard from './BodyStatsCard.svelte';
 
-	let { currentWeight, bf, desiredMuscleGain, pRatio } = $UserStore;
+	console.table($UserStore);
+	let { currentWeight, bf, desiredGains, pratio } = $UserStore;
 
 	const { totalWeightGain, endingWeight, fatGained, endingBodyFatPercent } =
-		calculateBodyComposition(pRatio, desiredMuscleGain, currentWeight, bf) as BodyComposition;
+		calculateBodyComposition(pratio, desiredGains, currentWeight, bf) as BodyComposition;
 
-
-		console.log({ totalWeightGain, endingWeight, fatGained, endingBodyFatPercent })
+	console.log({ totalWeightGain, endingWeight, fatGained, endingBodyFatPercent });
 </script>
 
-<section class="w-3/5 m-auto">
+<section class="w-11/12 m-auto">
 	<h2 class="mb-4">Body Composition Results</h2>
-	<p class="unstyled mb-4 font-medium text-lg">Gaining {desiredMuscleGain}lbs of muscle</p>
-	<ul >
-		<li class="bg-warning-500" ><p class="unstyled leading-10"><span class="font-semibold">Starting Weight: </span>{currentWeight}lbs</p></li>
-		<li class="bg-secondary-500"><p class="unstyled leading-10"><span class="font-semibold">Ending Weight: </span>{endingWeight}lbs</p></li>
-		<li class="bg-success-500"><p class="unstyled leading-10"><span class="font-semibold">Total Weight Gained: </span>{totalWeightGain}lbs</p></li>
-		<li  class="bg-warning-500"><p class="unstyled leading-10"><span class="font-semibold">Starting Body Fat: </span>{bf}%</p></li>
-		<li class="bg-secondary-500"><p class="unstyled leading-10"><span class="font-semibold">Ending Body Fat: </span>{endingBodyFatPercent}%</p></li>
-		<li  class="bg-error-500"><p class="unstyled leading-10"><span class="font-semibold">Expected Fat Gained: </span>{fatGained}lbs</p></li>
-	</ul>
-	
+	<p class="unstyled mb-4 font-medium text-lg text-center">Gaining {desiredGains}lbs of muscle</p>
 
+	<BodyStatsCard bodyFat={bf} bodyWeight={currentWeight} title="Starting Stats" colour="primary" />
+
+	<BodyStatsCard
+		bodyFat={endingBodyFatPercent}
+		bodyWeight={endingWeight}
+		title="Ending Stats"
+		colour="secondary"
+		{totalWeightGain}
+		{fatGained}
+	/>
+	<p class="unstyled mb-4 font-medium text-lg text-center">Time to gains: </p>
+	<ul>
+		<!-- <li class="bg-warning-500" /> -->
+		<!-- <li class="bg-secondary-500"><p class="unstyled leading-10"><span class="font-semibold">Ending Weight: </span>{endingWeight}lbs</p></li> -->
+		<!-- <li class="bg-success-500">
+			<p class="unstyled leading-10">
+				<span class="font-semibold">Total Weight Gained: </span>{totalWeightGain}lbs
+			</p>
+		</li> -->
+		<!-- <li  class="bg-warning-500"><p class="unstyled leading-10"><span class="font-semibold">Starting Body Fat: </span>{bf}%</p></li> -->
+		<!-- <li class="bg-secondary-500"><p class="unstyled leading-10"><span class="font-semibold">Ending Body Fat: </span>{endingBodyFatPercent}%</p></li> -->
+		<!-- <li class="bg-error-500">
+			<p class="unstyled leading-10">
+				<span class="font-semibold">Expected Fat Gained: </span>{fatGained}lbs
+			</p> -->
+		<!-- </li> -->
+	</ul>
 </section>
---
+++ src/lib/components/BodyStatsCard.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import { ProgressRadial } from '@skeletonlabs/skeleton';
+
+	export let bodyFat: number;
+	export let bodyWeight: number;
+	export let title: string;
+	export let colour: string;
+	export let totalWeightGain: number | null = null;
+	export let fatGained: number | null = null;
+</script>
+
+<div class="bg-white bg-opacity-10 m-2 rounded-2xl max-w-md p-4">
+	<h3>{title}</h3>
+	<div class="flex justify-between items-center">
+		<div class="flex flex-col items-center justify-between gap-2">
+			<p>Body Fat</p>
+			<ProgressRadial
+				value={bodyFat}
+				width="w-24"
+				font={108}
+				stroke={96}
+				meter="stroke-{colour}-500"
+				track="stroke-{colour}-500/30">{bodyFat}%</ProgressRadial
+			>
+		</div>
+		<div class="text-right">
+			<p class="unstyled leading-10">
+				<span class="font-semibold">Weight: </span>{bodyWeight}lbs
+			</p>
+				<p class="unstyled leading-10">
+					<span class="font-semibold">Lean Mass: </span>{totalWeightGain}lbs
+				</p>
+				<p class="unstyled leading-10">
+					<span class="font-semibold">Fat Mass: </span>{fatGained}lbs
+				</p>
+		</div>
+	</div>
+</div>
