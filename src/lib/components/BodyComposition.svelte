76cd20dcfa33bb8e572abdd9a11426b656ffb5de
Rebecca Burch <rebecca.burch.stu@gmail.com>
2023-04-16 20:27:17 -0500

    reusable stats added macro card

--- src/lib/components/BodyComposition.svelte
+++ src/lib/components/BodyComposition.svelte
@@ -5,8 +5,10 @@
 		calculateBodyComposition,
 		type BodyComposition
 	} from '../../utils/BodyCompositionResults';
-	import BodyStatsCard from './BodyStatsCard.svelte';
+	import StatsCard from './StatsCard.svelte';
 	import { calculateTimeToGains } from '../../utils/MuscleGainTimeCalculator';
+	import { each, init } from 'svelte/internal';
+	import { calculateMacronutrients } from '../../utils/NutritionCalculations';
 
 	let { currentWeight, initialBodyFatPercent, desiredGains, pratio } = $UserStore;
 
@@ -19,13 +21,37 @@
 		let result = weight * (bf * 0.01);
 		return result.toFixed(2);
 	}
-
+	$: macros = calculateMacronutrients(
+		$UserStore.currentWeight,
+		$UserStore.heightInInches,
+		$UserStore.age,
+		$UserStore.sex,
+		$UserStore.initialBodyFatPercent,
+		$UserStore.activityLevel
+	);
 	const { endingWeight, endingBodyFatPercent } = calculateBodyComposition(
 		pratio,
 		desiredGains,
 		currentWeight,
 		initialBodyFatPercent
 	) as BodyComposition;
+	const startCard = [
+		{ title: 'Weight', label: 'lbs', content: currentWeight },
+		{ title: 'Lean Mass', label: 'lbs', content: leanMass(currentWeight, initialBodyFatPercent) },
+		{ title: 'Fat Mass', label: 'lbs', content: fatMass(currentWeight, initialBodyFatPercent) }
+	];
+
+	const endCard = [
+		{ title: 'Weight', label: 'lbs', content: endingWeight },
+		{ title: 'Lean Mass', label: 'lbs', content: leanMass(endingWeight, endingBodyFatPercent) },
+		{ title: 'Fat Mass', label: 'lbs', content: fatMass(endingWeight, endingBodyFatPercent) }
+	];
+
+	$: macroCard = [
+		{ title: 'carbs', label: 'g', content: macros.carb },
+		{ title: 'protein', label: 'g', content: macros.protein },
+		{ title: 'fat', label: 'g', content: macros.fat }
+	];
 
 	$: timeToGains = calculateTimeToGains(
 		$UserStore.sex,
@@ -44,23 +70,53 @@
 <section class="w-11/12 m-auto">
 	<h2 class="mb-4">Body Composition Results</h2>
 	<p class="unstyled mb-4 font-medium text-lg text-center">Gaining {desiredGains}lbs of muscle</p>
-
-	<BodyStatsCard
-		bodyFat={endingBodyFatPercent}
-		bodyWeight={endingWeight}
-		fatLbs={fatMass(endingWeight, endingBodyFatPercent)}
-		leanLbs={leanMass(endingWeight, endingBodyFatPercent)}
-		title="Ending Stats"
-		colour="secondary"
-	/>
-	<BodyStatsCard
-		bodyFat={endingBodyFatPercent}
-		bodyWeight={endingWeight}
-		fatLbs={fatMass(currentWeight, initialBodyFatPercent)}
-		leanLbs={leanMass(currentWeight, initialBodyFatPercent)}
-		title="Starting Stats"
-		colour="primary"
-	/>
-
-	<p class="unstyled mb-4 font-medium text-lg text-center">Time to gains:</p>
+	<StatsCard title="Starting Stats">
+		<ProgressRadial
+			slot="one"
+			value={initialBodyFatPercent}
+			width="w-24"
+			font={108}
+			stroke={96}
+			meter="stroke-primary-500"
+			track="stroke-primary-500/30">{initialBodyFatPercent}%</ProgressRadial
+		>
+		<div slot="two">
+			<p>Body Fat</p>
+			{#each startCard as item}
+				<p class="unstyled leading-10">
+					<span class="font-semibold">{item.title}: </span>{item.content + ' ' + item.label}
+				</p>
+			{/each}
+		</div>
+	</StatsCard>
+	<StatsCard title="Starting Stats">
+		<ProgressRadial
+			slot="one"
+			value={endingBodyFatPercent}
+			width="w-24"
+			font={108}
+			stroke={96}
+			meter="stroke-secondary-500"
+			track="stroke-secondary-500/30">{endingBodyFatPercent}%</ProgressRadial
+		>
+		<div slot="two">
+			<p>Body Fat</p>
+			{#each endCard as item}
+				<p class="unstyled leading-10">
+					<span class="font-semibold">{item.title}: </span>{item.content + ' ' + item.label}
+				</p>
+			{/each}
+		</div>
+	</StatsCard>
+	<StatsCard title=" Macros">
+		<div slot="two">
+			<p>Macros</p>
+			{#each macroCard as item}
+				<p class="unstyled leading-10">
+					<span class="font-semibold">{item.title}: </span>{item.content + ' ' + item.label}
+				</p>
+			{/each}
+		</div>
+	</StatsCard>
+	<p class="unstyled mb-4 font-medium text-lg text-center">Time to gains: {estimatedMessage}</p>
 </section>
--- src/lib/components/StatsCard.svelte
+++ src/lib/components/StatsCard.svelte
@@ -1,27 +1,17 @@
 <script lang="ts">
-	import { ProgressRadial } from '@skeletonlabs/skeleton';
-	export let percentVal: number;
+	import { ProgressRadial, SlideToggle } from '@skeletonlabs/skeleton';
 	export let title: string;
-	export let colour: string;
 </script>
 
 <div class="bg-white bg-opacity-10 m-2 rounded-2xl max-w-md p-4">
 	<h3>{title}</h3>
 	<div class="flex justify-between items-center">
 		<div class="flex flex-col items-center justify-between gap-2">
-			<p>Body Fat</p>
-			<ProgressRadial
-				value={percentVal}
-				width="w-24"
-				font={108}
-				stroke={96}
-				meter="stroke-{colour}-500"
-				track="stroke-{colour}-500/30">{percentVal}%</ProgressRadial
-			>
+			<slot name="one"><!-- optional fallback --></slot>
 		</div>
 		<div class="text-right">
 			<p class="unstyled leading-10">
-				<slot />
+				<slot name="two" />
 			</p>
 		</div>
 	</div>
